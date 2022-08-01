b692e740495a5047b873c1e1c5beaa45572783ac
BuddhiLW <pedrogbranquinho@gmail.com>
2022-08-01 18:30:57 -0300

    Add animation with no replacement

--- core.jl
+++ core.jl
@@ -5,7 +5,7 @@ using Plots;
 pyplot();
 
 deck = ["A" => [[1, 11], 4], "C2" => [2, 4], "C3" => [3, 4], "C4" => [4, 4], "C5" => [5, 4], "C6" => [6, 4], "C7" => [7, 4], "C8" => [8, 4], "C9" => [9, 4], "C10" => [10, 4], "CJ" => ["J", 4], "CQ" => ["Q", 4], "CK" => ["K", 4]]
-cards = ["A", "2", "3", "4", "6", "7", "8", "9", "10", "J", "Q", "K"]
+cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 pcards = 13                       # possible cards
 
 function cardEvent()
@@ -31,21 +31,23 @@ bar([keys(frequency)...], values_norm, title=L"\mathbf{20 retiradas}")
 using DataStructures
 N = 1 * 10^3
 frequency2 = [cardEvent() for _ in 1:N]
-anim = @animate for i = 1:N
-    if i < 10
-        f = counter(frequency2[1:i])
-        P = [j / sum(f) for j in values(f)]
-        title = "$i retiradas"
-        bar([keys(f)...], P, title=title)
-    elseif mod(i, 10) == 0
-        f = counter(frequency2[1:i])
-        P = [j / sum(f) for j in values(f)]
-        title = "$i retiradas"
-        bar([keys(f)...], P, title=title)
-    end
+anim = @animate for i = 1:5:N
+    f = counter(frequency2[1:i])
+    P = [j / sum(f) for j in values(f)]
+    title = "$i retiradas"
+    bar([keys(f)...], P, title=title)
 end
 
-gif(anim, "mygif.gif", fps=10)
+gif(anim, "mygif.gif", fps=40)
+
+
+function cardEventNR(n=1, dks=6, r=false)
+    c = repeat(cards, 4 * dks) # 4 cards per number of decks per card-type
+    hits = sample(c, n, replace=r)
+    return hits
+end
+
+frequency3 = cardEventNR(312, 6)
 
 # for
 
